db5ee85487e88436538c07969aaafdd694348a69
lucas.j <lucas.j@hpcnt.com>
2021-08-13 21:44:22 +0900

    cargo nice

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,14 @@
 mod print; 
 mod vars;
 mod types;
+mod string;
 
 fn main() {
     println!("Hello, world!");
     print::run();
     vars::vars();
     types::types();
-
+    string::string_fn();
+    string::run();
     
 }
---
+++ src/string.rs
@@ -0,0 +1,6 @@
+pub fn string_fn(){
+
+}
+pub fn run(){
+  println!("this is inside of string")
+}
